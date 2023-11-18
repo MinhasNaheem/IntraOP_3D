6ab1d126de2839cb740ba8e54aa9b214351070a3
Praveen Ez <praveen.e@perfinthealthcare.com>
2023-11-18 11:04:05 +0530

    code to find the offset between the vertex and the divot added

--- getCameraData.py
+++ getCameraData.py
@@ -1,4 +1,3 @@
-import requests
 import numpy as np
 from MathFunctions import createTransformationMatrix,pointBasedRegistration
 from CameraData import CameraData
@@ -9,16 +8,22 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 np.set_printoptions(suppress=True)
 import os
-vertex_divot = np.loadtxt('D:\IntraOP_3D\divot_vertex.txt')
-center_divot= np.loadtxt('D:\IntraOP_3D\divot_centre.txt')
+import keyboard
+offset_errors = [] 
+# load the cmm points of the vertex and centre
+# vertex_divot = np.loadtxt('new_vertex_position.txt')
+# center_divot= np.loadtxt('new_center_position.txt')
+
+vertex_divot=np.loadtxt("D:\\IntraOP_3D\\ASTM_data\\vertex.txt")
+center_divot=np.loadtxt("D:\\IntraOP_3D\\ASTM_data\\center.txt")
+# function to label the points in the plot
 def label_points(points, ax):
     for i, (x, y, z) in enumerate(points):
-        # (x,y,z) refers to the position where the text is to be placed
         ax.text(x, y, z,str(i + 1))
 
 
-
-def updateData(toolMarker,refMarker,ax):
+# function for finding tooltip position
+def updateData(toolMarker,refMarker):
     cam = CameraData(toolMarker, refMarker)
     jsonData = cam.GetCameraData()
     markerData = cam.parseCameraData(jsonData)
@@ -31,54 +36,88 @@ def updateData(toolMarker,refMarker,ax):
         ref2tool_tf = np.linalg.inv(tool2cam_tf) @ ref2cam_tf
         tool2ref_tf = np.linalg.inv(ref2tool_tf)
         tooltip_position=tool2ref_tf[:3,3]
-        ax.scatter(tool2ref_tf[0,3],tool2ref_tf[1,3],tool2ref_tf[2,3],c='g', marker='o', label='tool tip position in the divot')
         tooltip2vertexdist = []
         for i in range(len(vertex_divot)):
             
             distance=np.linalg.norm(vertex_divot[i]-tooltip_position)
             tooltip2vertexdist.append(distance)
+            
         vertex=vertex_divot[np.argmin(tooltip2vertexdist)]
         centre=center_divot[np.argmin(tooltip2vertexdist)]
         offset=np.linalg.norm(tool2ref_tf[:3,3]-vertex)
         dist_vector=centre-vertex
-        print("offset between the vertex and the tool tip",offset)
         projection=((np.dot((tooltip_position-vertex),dist_vector)/np.square(np.linalg.norm(dist_vector))))
-        # tipRadius=
         # pointOffset = (np.sqrt(2)-1)* tipRadius
         # offsetVector = np.array([pointOffset,0,0])
         # print("projection/displacement of the tip from the axis of the divot",projection)
-        ax.quiver(vertex[0],vertex[1],vertex[2],centre[0]-vertex[0],centre[1]-vertex[1],centre[2]-vertex[2])
+        return tooltip_position
         
-        # print(tool2ref_tf)
-
-if __name__ == "__main__":
-    toolMarker = "11002"
-    refMarker = "4321"
+# keyboard event 
+def on_key_event(e):
+    global tooltip_position_vertex,vertex,center,offset_correction
+    divot_index = e - 1
+    if 0 <= divot_index < len(vertex_divot):
+        vertex = vertex_divot[divot_index]
+        center = center_divot[divot_index]
+        tooltip_position_vertex = updateData(toolMarker, refMarker)
+        offset_errors.append(np.linalg.norm(tooltip_position_vertex - vertex))
+        a=center-tooltip_position_vertex
+        print(f"Divot {divot_index + 1}:")
+        print(f"tooltip placed in vertex:{tooltip_position_vertex}")
+        print(f"Offset between the tooltip and the vertex: {np.linalg.norm(tooltip_position_vertex - vertex)}")
+        # plot()
+        # time.sleep(5)
+        
+        # tooltip_position_in_center=updateData(toolMarker,refMarker)
+        # r=tooltip_position_in_center-center
+        # offset_correction=a+r
+        # print(f"tooltip placed in center:{tooltip_position_in_center}")
+        # print(f"offset correction for Divot {divot_index+1}",offset_correction)
+        # return offset_correction
+        
     
+def plot():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    ax.view_init(elev=30, azim=45, roll=0)
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
-    ax.set_xlim3d(0, 15)
-    ax.set_ylim3d(0, 15)
-    ax.set_zlim3d(0, 15)
+    ax.set_xlim3d(vertex[0]+5, vertex[0]-5)
+    ax.set_ylim3d(vertex[1]+5, vertex[1]-5)
+    ax.set_zlim3d(vertex[2]+5, vertex[2]-5)
     
-    
-    def update(frame):
-        ax.clear()
-        ax.scatter(vertex_divot[:,0], vertex_divot[:,1], vertex_divot[:,2], c='b', marker='o', label='Vertex Points')
-        ax.scatter(center_divot[:,0], center_divot[:,1], center_divot[:,2], c='r', marker='o', label='Center Points')
-        label_points(vertex_divot, ax)
-        label_points(center_divot, ax)
-        updateData(toolMarker, refMarker, ax)
-        
-    
-        ax.legend()
-    
-    ani = FuncAnimation(fig, update, frames=100, interval=500)  # Adjust frames and interval as needed
+    ax.scatter(vertex[0], vertex[1], vertex[2], c='b', marker='o', label='Vertex Points')
+    ax.scatter(center[0], center[1], center[2], c='r', marker='o', label='Center Points')
+    ax.quiver(vertex[0],vertex[1],vertex[2],center[0]-vertex[0],center[1]-vertex[1],center[2]-vertex[2])
+    ax.scatter(tooltip_position_vertex[0],tooltip_position_vertex[1],tooltip_position_vertex[2],c='g', marker='o', label='tool tip position in the divot')
     plt.show()
-   
+    return fig
+
+if __name__ == "__main__":
+    # toolMarker = "11002"
+    toolMarker = "51001"
+    refMarker = "4321"
+    iteration_count=0
+    number_of_divots = 18
+    while iteration_count<number_of_divots:
+        x =int(input("press a key between 1 to 18: "))
+        # tooltip_position_in_vertex=updateData(toolMarker,refMarker)
+        # print("tool tip position in divot vertex=",tooltip_position_in_vertex)
+        # a=center_divot-tooltip_position_in_vertex
+        # print(a)
+        # time.sleep(3)
+        # tooltip_position_in_center=updateData(toolMarker,refMarker)
+        # print("tool tip position in divot center=",tooltip_position_in_center)
+        # r=tooltip_position_in_center-center_divot
+        # offset_correction=a+r
+        # vertex,center,offset_from_vertex=on_key_event(x)
         
-        
+        offset_correction=on_key_event(x)
+        plot()
+        iteration_count +=1 
+mean_offset=np.mean(offset_errors)
+# print("mean offset from the vertex :",mean_offset)
+# print("offset correction : ",offset_correction)
+
 
--- test_divot.py
+++ test_divot.py
@@ -1,15 +1,17 @@
-import requests
 import numpy as np
 from MathFunctions import createTransformationMatrix,pointBasedRegistration
 from CameraData import CameraData
 import time
 import matplotlib.pyplot as plt
+from functions import Registration
 from mpl_toolkits import mplot3d
 
 np.set_printoptions(suppress=True)
 import os
 vertex_divot = np.loadtxt('D:\IntraOP_3D\divot_vertex.txt')
 center_divot= np.loadtxt('D:\IntraOP_3D\divot_centre.txt')
+# vertex_divot = np.loadtxt('new_vertex_position.txt')
+# center_divot= np.loadtxt('new_center_position.txt')
 def label_points(points, ax):
     for i, (x, y, z) in enumerate(points):
         # (x,y,z) refers to the position where the text is to be placed
@@ -30,8 +32,11 @@ def updateData(toolMarker,refMarker,ax):
         ref2tool_tf = np.linalg.inv(tool2cam_tf) @ ref2cam_tf
         tool2ref_tf = np.linalg.inv(ref2tool_tf)
         tooltip_position=tool2ref_tf[:3,3]
-        reference_original=np.array([-83.9394,27.6742,56.7004],[-98.1793,67.7947,14.5623],[-18.6323,139.084,-1.74854],[23.069,81.072,71.306])
-        reference_new=
+        
+        
+        
+           
+       
         ax.scatter(tool2ref_tf[0,3],tool2ref_tf[1,3],tool2ref_tf[2,3],c='g', marker='o', label='tool tip position in the divot')
         tooltip2vertexdist = []
         for i in range(len(vertex_divot)):
@@ -49,31 +54,48 @@ def updateData(toolMarker,refMarker,ax):
         # offsetVector = np.array([pointOffset,0,0])
         print("projection",projection)
         ax.quiver(vertex[0],vertex[1],vertex[2],centre[0]-vertex[0],centre[1]-vertex[1],centre[2]-vertex[2])
+        return tooltip_position
         
         
+toolMarker = "51001"
+refMarker = "4321"
+# divots in the original space
+original_divots=np.array(([-14.3688,-0.104038,-1.81399],[0.182278,14.452,-1.83422],[-28.7293,28.8883,-1.5746]))
+tool_positions = []
+iteration_count=0
+point_count_cmm = 4
+while iteration_count<point_count_cmm:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(vertex_divot[:,0], vertex_divot[:,1], vertex_divot[:,2], c='b', marker='o', label='Vertex Points')
+    ax.scatter(center_divot[:,0], center_divot[:,1], center_divot[:,2], c='r', marker='o', label='Center Points')
+    label_points(vertex_divot, ax)
+    label_points(center_divot, ax)
+    tooltip_position = updateData(toolMarker, refMarker, ax)
+    
+    # Store the first three tool positions
+    if len(tool_positions) < point_count_cmm:
+        tool_positions.append(tooltip_position)
+        
+    # np.savetxt("tool_tip_position.csv",tool_positions)
 
-if __name__ == "__main__":
-    toolMarker = "11002"
-    refMarker = "4321"
-   
-    while True:
-        
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(vertex_divot[:,0],vertex_divot[:,1],vertex_divot[:,2],c='b', marker='o', label='Vertex Points')
-        ax.scatter(center_divot[:,0],center_divot[:,1],center_divot[:,2], c='r', marker='o', label='Center Points')
-        label_points(vertex_divot, ax)
-        label_points(center_divot, ax)
-        updateData(toolMarker,refMarker,ax)
-        ax.set_xlabel('X Label')
-        ax.set_ylabel('Y Label')
-        ax.set_zlabel('Z Label')    
-        ax.legend()
-        ax.set_xlim([min(vertex_divot[:, 0]), max(vertex_divot[:, 0])])
-        ax.set_ylim([min(vertex_divot[:, 1]), max(vertex_divot[:, 1])])
-        ax.set_zlim([min(vertex_divot[:, 2]), max(vertex_divot[:, 2])])
-        
-        plt.show()
-        # plt.pause(5)
-        # plt.close()
-        time.sleep(0.5)
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')    
+    ax.legend()
+    ax.set_xlim([min(vertex_divot[:, 0]), max(vertex_divot[:, 0])])
+    ax.set_ylim([min(vertex_divot[:, 1]), max(vertex_divot[:, 1])])
+    ax.set_zlim([min(vertex_divot[:, 2]), max(vertex_divot[:, 2])])
+    
+    plt.show()
+    time.sleep(0.5)
+    iteration_count +=1 
+    
+# data=np.loadtxt(r"D:\IntraOP_3D\tool_tip_position.csv")
+tf,err=Registration(vertex_divot[:point_count_cmm,:],tool_positions)
+print("resgitration error=",err)
+# new_vertex_position=(tf@(np.concatenate((vertex_divot, np.ones((18,1))), axis=1)).T).T
+# new_center_position=(tf@(np.concatenate((center_divot, np.ones((18,1))), axis=1)).T).T
+# np.savetxt("new_vertex_position.txt",new_vertex_position[:,0:3])
+# np.savetxt("new_center_position.txt",new_center_position[:,0:3])
+
